3e80f7d61aa2152af0e0f5776074e818f83da658
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-06-23 16:05:29 -0700

    Rendering Recipe Info

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,15 +1,11 @@
 import { sanityClient, urlFor, usePreviewSubscription, PortableText } from '../../lib/sanity'
+import ingredient from '../../studio/schemas/ingredient';
 
 const recipeQuery = `*[_type == "recipe" && slug.current == $slug][0]{
   _id,
   name,
   slug,
-  mainImage{
-    asset->{
-      _id,
-      url
-    }
-  },
+  mainImage,
   ingredient[]{
     unit,
     wholeNumber,
@@ -21,16 +17,51 @@ const recipeQuery = `*[_type == "recipe" && slug.current == $slug][0]{
   instructions
 }`
 
-export default function OneRecipe() {
-
+export default function OneRecipe({ data }) {
+  const { recipe } = data;
+  return (
+    <article className="recipe">
+      <h1>{recipe.name}</h1>
+      <main className="content">
+        <img src={urlFor(recipe?.mainImage).url()} alt={recipe.name} />
+          <div className="breakdown">
+            <ul className="ingredients">
+              {recipe.ingredient?.map((ingredient) => (
+                <li key={ingredient._key} className="ingredient">
+                  {ingredient?.wholeNumber}
+                  {ingredient?.fraction}
+                  {" "}
+                  {ingredient?.unit}
+                  <br />
+                  {ingredient?.ingredient?.name}
+                </li>
+              ))}
+            </ul>
+            <PortableText blocks={recipe?.instructions} className="instructions" />
+          </div>
+      </main>
+    </article>
+  )
 }
 
 
 export async function getStaticPaths() {
-
+  const paths = await sanityClient.fetch(
+    `*[_type == "recipe" && defined(slug.current)]{
+      "params": {
+        "slug": slug.current
+      }
+    }`
+  )
+  return {
+    paths,
+    fallback: true
+  }
 }
 
-export async function getStaticProps() {
-
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+  const recipe = await sanityClient.fetch(recipeQuery, {slug})
+  return { props: { data: { recipe }}}
 }
 
